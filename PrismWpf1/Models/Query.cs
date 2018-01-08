cacf7ba421d1272315c7b0aae19fe58e62d7df63
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2018-01-08 21:30:07 +0900

    SQLite

---
+++ PrismWpf1/Models/Query.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismWpf1.Models
+{
+    public class Query
+    {
+        private static DataModule.Database db;
+        public static DataModule.Database DB { get { return DB ?? (db = new DataModule.Database()); } }
+
+        public IQueryable<DataModule.MyEntity> MyEntities()
+        {
+            return DB.MyEntities;
+        }
+    }
+}
